3b0c7226a18191b86ecdc9e6fbfd1a2cc9bc5125
cmhedrick <christopher.m.hedrick@gmail.com>
2019-03-27 15:48:37 -0400

    setting up dumb models

--- qna_site/qna_app/admin.py
+++ qna_site/qna_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from qna_app import models
+
+admin.site.register(models.Organization)
+admin.site.register(models.Staff)
+admin.site.register(models.Role)
--- qna_site/qna_app/models.py
+++ qna_site/qna_app/models.py
@@ -1,3 +1,74 @@
-from django.db import models
+from __future__ import unicode_literals
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Organization(models.Model):
+    org_logo = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to='profile/%Y/%m/%d'
+    )
+    phone_number = models.CharField(
+        max_length=12,
+        default='XXXXXXXXXX'
+    )
+    org_name = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False,
+        default="organization name"
+    )
+
+    def __unicode__(self):
+        return "%s" % (self.org_name)
+
+    def __str__(self):
+        return "%s" % (self.org_name)
+
+
+class Role(models.Model):
+    role = models.CharField(
+        max_length=32,
+        blank=False,
+        null=False,
+        default="role"
+    )
+
+    def __unicode__(self):
+        return "%s" % (self.role)
+
+    def __str__(self):
+        return "%s" % (self.role)
+
+
+class Staff(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="staff",
+        null=True
+    )
+    role = models.ManyToManyField(Role)
+    org = models.ManyToManyField(Organization)
+    profile_pic = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to='profile/%Y/%m/%d'
+        )
+    phone_number = models.CharField(
+        max_length=12,
+        default='XXXXXXXXXX'
+    )
+
+    @property
+    def email(self):
+        return self.user.email
+
+    def __unicode__(self):
+        return "%s %s" % (self.user.first_name, self.user.last_name)
+
+    def __str__(self):
+        return "%s %s" % (self.user.first_name, self.user.last_name)
